5ec16c3007b04808c2164872527a1d67d4dee3a1
alandgmendes <adgmendes@gmail.com>
2023-03-09 11:07:45 -0300

    trying to raise

--- src/FreeComponent.js
+++ src/FreeComponent.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PrintComponent from "./PrintComponent";
 
 export default function FreeComponent() {
   // set an initial state for the message we will receive after the API call
@@ -32,7 +31,7 @@ export default function FreeComponent() {
       {/* displaying our message from our API call */}
       <h3 className="text-center text-danger">{message}</h3>
 
-      <PrintComponent />
+
     </div>
   );
 }
